4238634a03938da5898e355b32b4377e15cc15a4
Alan Kofi Safo Ofori <144915821+oforias@users.noreply.github.com>
2024-12-02 15:59:48 -0300

    here you go

--- MyForm.h
+++ MyForm.h
@@ -21,7 +21,8 @@ namespace ashesi {
 		DataTable^ sqlDt =  gcnew DataTable();
 		MySqlDataAdapter^ sqlDtA = gcnew MySqlDataAdapter();
 	private: System::Windows::Forms::Button^ btn1Cancel;
-	private: System::Windows::Forms::ContextMenuStrip^ contextMenuStrip1;
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
+
 
 
 
@@ -92,84 +93,112 @@ namespace ashesi {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = (gcnew System::ComponentModel::Container());
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->passwd = (gcnew System::Windows::Forms::Label());
 			this->btnLogin = (gcnew System::Windows::Forms::Button());
 			this->txtEmail = (gcnew System::Windows::Forms::TextBox());
 			this->txtPassword = (gcnew System::Windows::Forms::TextBox());
 			this->btn1Cancel = (gcnew System::Windows::Forms::Button());
-			this->contextMenuStrip1 = (gcnew System::Windows::Forms::ContextMenuStrip(this->components));
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// label1
 			// 
 			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(129, 211);
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->ForeColor = System::Drawing::SystemColors::Control;
+			this->label1->Location = System::Drawing::Point(335, 99);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(52, 20);
+			this->label1->Size = System::Drawing::Size(66, 25);
 			this->label1->TabIndex = 0;
 			this->label1->Text = L"Email:";
 			// 
 			// passwd
 			// 
 			this->passwd->AutoSize = true;
-			this->passwd->Location = System::Drawing::Point(129, 307);
+			this->passwd->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->passwd->ForeColor = System::Drawing::SystemColors::Control;
+			this->passwd->Location = System::Drawing::Point(297, 181);
 			this->passwd->Name = L"passwd";
-			this->passwd->Size = System::Drawing::Size(82, 20);
+			this->passwd->Size = System::Drawing::Size(104, 25);
 			this->passwd->TabIndex = 1;
 			this->passwd->Text = L"Password:";
 			this->passwd->Click += gcnew System::EventHandler(this, &MyForm::label2_Click);
 			// 
 			// btnLogin
 			// 
-			this->btnLogin->Location = System::Drawing::Point(353, 363);
+			this->btnLogin->BackColor = System::Drawing::Color::LightCoral;
+			this->btnLogin->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->btnLogin->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->btnLogin->ForeColor = System::Drawing::SystemColors::ActiveCaptionText;
+			this->btnLogin->Location = System::Drawing::Point(407, 264);
 			this->btnLogin->Margin = System::Windows::Forms::Padding(3, 4, 3, 4);
 			this->btnLogin->Name = L"btnLogin";
-			this->btnLogin->Size = System::Drawing::Size(106, 59);
+			this->btnLogin->Size = System::Drawing::Size(91, 37);
 			this->btnLogin->TabIndex = 7;
 			this->btnLogin->Text = L"Login";
-			this->btnLogin->UseVisualStyleBackColor = true;
+			this->btnLogin->UseVisualStyleBackColor = false;
 			this->btnLogin->Click += gcnew System::EventHandler(this, &MyForm::btnLogin_Click);
 			// 
 			// txtEmail
 			// 
-			this->txtEmail->Location = System::Drawing::Point(353, 205);
+			this->txtEmail->ForeColor = System::Drawing::SystemColors::Control;
+			this->txtEmail->Location = System::Drawing::Point(407, 98);
 			this->txtEmail->Name = L"txtEmail";
-			this->txtEmail->Size = System::Drawing::Size(281, 26);
+			this->txtEmail->Size = System::Drawing::Size(211, 26);
 			this->txtEmail->TabIndex = 8;
 			this->txtEmail->TextChanged += gcnew System::EventHandler(this, &MyForm::txtFName_TextChanged);
 			// 
 			// txtPassword
 			// 
-			this->txtPassword->Location = System::Drawing::Point(353, 301);
+			this->txtPassword->ForeColor = System::Drawing::SystemColors::Control;
+			this->txtPassword->Location = System::Drawing::Point(407, 180);
 			this->txtPassword->Margin = System::Windows::Forms::Padding(3, 4, 3, 4);
 			this->txtPassword->Name = L"txtPassword";
-			this->txtPassword->Size = System::Drawing::Size(281, 26);
+			this->txtPassword->Size = System::Drawing::Size(211, 26);
 			this->txtPassword->TabIndex = 9;
 			// 
 			// btn1Cancel
 			// 
-			this->btn1Cancel->Location = System::Drawing::Point(528, 363);
+			this->btn1Cancel->BackColor = System::Drawing::Color::Red;
+			this->btn1Cancel->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Center;
+			this->btn1Cancel->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->btn1Cancel->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->btn1Cancel->ForeColor = System::Drawing::SystemColors::ActiveCaptionText;
+			this->btn1Cancel->Location = System::Drawing::Point(527, 264);
 			this->btn1Cancel->Margin = System::Windows::Forms::Padding(3, 4, 3, 4);
 			this->btn1Cancel->Name = L"btn1Cancel";
-			this->btn1Cancel->Size = System::Drawing::Size(106, 59);
+			this->btn1Cancel->Size = System::Drawing::Size(91, 37);
 			this->btn1Cancel->TabIndex = 10;
 			this->btn1Cancel->Text = L"Cancel";
-			this->btn1Cancel->UseVisualStyleBackColor = true;
+			this->btn1Cancel->UseVisualStyleBackColor = false;
 			this->btn1Cancel->Click += gcnew System::EventHandler(this, &MyForm::btn1Cancel_Click);
 			// 
-			// contextMenuStrip1
+			// pictureBox1
 			// 
-			this->contextMenuStrip1->ImageScalingSize = System::Drawing::Size(24, 24);
-			this->contextMenuStrip1->Name = L"contextMenuStrip1";
-			this->contextMenuStrip1->Size = System::Drawing::Size(61, 4);
+			this->pictureBox1->BackColor = System::Drawing::SystemColors::ActiveCaption;
+			this->pictureBox1->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Center;
+			this->pictureBox1->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
+			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
+			this->pictureBox1->Location = System::Drawing::Point(33, 67);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(244, 259);
+			this->pictureBox1->TabIndex = 11;
+			this->pictureBox1->TabStop = false;
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(9, 20);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(788, 656);
+			this->BackColor = System::Drawing::Color::Maroon;
+			this->ClientSize = System::Drawing::Size(678, 405);
+			this->Controls->Add(this->pictureBox1);
 			this->Controls->Add(this->btn1Cancel);
 			this->Controls->Add(this->txtPassword);
 			this->Controls->Add(this->txtEmail);
@@ -178,8 +207,9 @@ namespace ashesi {
 			this->Controls->Add(this->label1);
 			this->Margin = System::Windows::Forms::Padding(3, 4, 3, 4);
 			this->Name = L"MyForm";
-			this->Text = L"MyForm";
+			this->Text = L"Login Page";
 			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
